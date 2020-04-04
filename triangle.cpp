36a7f21cc14a7893ed7ec7599c247c5df29199c0
ShadenAlZaid <62605834+ShadenAlZaid@users.noreply.github.com>
2020-04-04 11:34:56 +0300

    Add files via upload

--- triangle.cpp
+++ triangle.cpp
@@ -1,74 +1,53 @@
-//
-//  triangle.cpp
-//  Interaction in OpenGl using Mouse
-//
-//  Created by UNI on 4/2/20.
-//  Copyright © 2020 jennan sowayan. All rights reserved.
-//
 
-#include "triangle.hpp"
 #include <iostream>
 #include <OpenGL/gl.h>
 #include <GLUT/glut.h>
 #include <stdlib.h>
 #include <string.h>
 
-float r, g, b, x, y, x1, x2, x3, y1, y2, y3;
-bool check = true;
+int flag = 0;
+int x1 = 0;
+int y1 = 0;
 
-void mouse(int button, int state, int x, int y)
-{
-    if(button==GLUT_LEFT_BUTTON && state==GLUT_DOWN)
-    {
-       check = true;
-        int x1 = x;
-        int y1 = 600 - y;
-
-         r=(rand()%9)/8;
-         g=(rand()%9)/8;
-         b=(rand()%9)/8;
-
-         x2 = x1 - 50;
-         y2 = y1 - 50;
-
-         x3 = x1 + 50;
-         y3 = y1 + 50;
-     }
-}
-
-void DrawTriangle(){
-           glColor3f(r,g,b);
-
-            glMatrixMode(GL_PROJECTION);
-            glLoadIdentity();
-            gluOrtho2D(0.0, 600.0, 0.0, 600.0);
-        
-    
-    if(check)
-        {
-        glPointSize(50);
-        
+void DrawTriangle (void) {
+      if (flag == 1){
+        glColor3f(1,1,1); // sets color of triangle to white
         glBegin(GL_TRIANGLES);
-        
         glVertex2i(x1, y1);
-        glVertex2i(x2, y2);
-        glVertex2i(x3, y3);
-
-        
+        glVertex2i(x1, y1 + 50);
+        glVertex2i(x1 - 50, y1);
         glEnd();
-        glFlush();
-    }
+      }
 
+
+    glFlush();
 }
 
-int main()
-{
-    glutInitWindowSize(640, 600);   //sets the width and height of the window in pixels
-    glutInitDisplayMode(GLUT_RGB);//creates a single frame buffer of RGB color capacity.
-    glutCreateWindow("TRIANGLE");//creates the window as specified by the user as above.
-    glClearColor(0, 0, 0, 1);
-    glClear(GL_COLOR_BUFFER_BIT);
+void Mouse (int button, int state, int x, int y) {
+    
+        if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN){ // if left button is clicked down
+         x1 = x;
+         y1 = y;
+         flag = 1; // set flag to 1  to draw triangle
+         glutPostRedisplay();
+
+}
+}
+
+int main(int argc, char** argv) {
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB);
+    glutInitWindowSize(600, 600);
+    glutCreateWindow("TRIANGLES");
     glutDisplayFunc(DrawTriangle);
-    glutMouseFunc(mouse);
+
+    glutMouseFunc(Mouse);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0, 600, 600, 0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
     glutMainLoop();
+    return 0;
 }
